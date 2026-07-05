883e5eaee214a796ce1be58ed0bdbc8d086bf2fb
gargey.7 <saigargey007@gmail.com>
2026-07-05 18:40:04 +0530

    Fix: use configurable base URL from environment variable

--- src/main/java/com/url/url_shortner/controller/UrlController.java
+++ src/main/java/com/url/url_shortner/controller/UrlController.java
@@ -2,11 +2,11 @@ package com.url.url_shortner.controller;
 
 import com.url.url_shortner.repository.UrlRepository;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.url.url_shortner.model.Url;
 import com.url.url_shortner.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +24,9 @@ public class UrlController {
     @Autowired
     private UrlRepository urlRepository;
 
+    @Value("${APP_BASE_URL:http://localhost:8081}")
+    private String baseUrl;
+
     @PostMapping("/api/shorten")
     public ResponseEntity<Map<String, Object>> shortenUrl(@RequestBody Map<String, String> request) {
         String originalUrl = request.get("url");
@@ -33,7 +36,7 @@ public class UrlController {
         Map<String, Object> response = new HashMap<>();
         response.put("shortCode", url.getShortCode());
         response.put("originalUrl", url.getOriginalUrl());
-        response.put("shortUrl", "http://localhost:8081/" + url.getShortCode());
+        response.put("shortUrl", baseUrl + "/" + url.getShortCode());
         response.put("clickCount", url.getClickCount());
 
         return ResponseEntity.ok(response);
@@ -48,6 +51,7 @@ public class UrlController {
 
         return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
     }
+
     @GetMapping("/api/urls")
     public ResponseEntity<List<Map<String, Object>>> getAllUrls() {
         List<Map<String, Object>> urls = urlRepository.findAll().stream()
@@ -55,7 +59,7 @@ public class UrlController {
                     Map<String, Object> map = new HashMap<>();
                     map.put("shortCode", url.getShortCode());
                     map.put("originalUrl", url.getOriginalUrl());
-                    map.put("shortUrl", "http://localhost:8081/" + url.getShortCode());
+                    map.put("shortUrl", baseUrl + "/" + url.getShortCode());
                     map.put("clickCount", url.getClickCount());
                     return map;
                 })
